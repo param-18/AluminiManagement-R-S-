480b20a2c94143c17f8d06e7ba4c7031872e67e4
Param <param-18>
2021-10-18 18:56:13 +0530

    initialized project for spring boot

---
+++ src/main/java/com/prcp/aluminimanagementsystem/AluminiManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.prcp.aluminimanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluminiManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluminiManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prcp/aluminimanagementsystem/AluminiManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prcp.aluminimanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluminiManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
